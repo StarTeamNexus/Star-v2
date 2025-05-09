2d1b3857c686f17191757fad6d7b060bb3ed4c2d
Scaro <123261052+RipAngelhacks@users.noreply.github.com>
2025-05-09 07:19:15 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    cdnBase: 'https://raw.githubusercontent.com/StarTeamNexus/files/main/',
+    defaultIcon: '.icons/idk',
+    apiEndpoints: {
+        gameList: 'list.json',
+        icons: 'icons/'
+    }
+};
